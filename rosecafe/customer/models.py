53836692a63c993bd6b8c4169ea040fb6a673f8c
rosiethesupergeek <rosiethesupergeek.dev@gmail.com>
2023-03-22 16:41:35 +0000

    end of day commit

--- rosecafe/customer/models.py
+++ rosecafe/customer/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+class menuItem(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to=)
+
 # Create your models here.
