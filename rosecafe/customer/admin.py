10fba09ce6c92c35a9a2a644982c4018c1d0b8bf
rosiethesupergeek <rosiethesupergeek.dev@gmail.com>
2023-03-24 21:25:15 +0000

    create models for menuitem, category, order

--- rosecafe/customer/admin.py
+++ rosecafe/customer/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import MenuItem, Category, OrderModel
+
+admin.site.register(MenuItem)
+admin.site.register(Category)
+admin.site.register(OrderModel)
 
 # Register your models here.
--- rosecafe/customer/views.py
+++ rosecafe/customer/views.py
@@ -9,4 +9,26 @@ class About(View):
     def get(self, request, *args, **kwargs):
         return render(request, 'customer/about.html')
 
+class Order(View):
+    def get(self, request, *args, **kwargs):
+        #get every item from each category
+        appetizers = MenuItem.objects.filter(category__name__contains='Appetizer')
+        entres = MenuItem.objects.filter(category__name__contains='Entre')
+        desserts = MenuItem.objects.filter(category__name__contains='Dessert')
+        drinks = MenuItem.objects.filter(category__name__contains='Drink')
+
+        #pass into context
+        context = {
+            'appetizers': appetizers,
+            'entres': entres,
+            'desserts': desserts,
+            'drinks': drinks
+        }
+
+        #render the template
+        return render(request, 'customer/order.html', context)
+
+        def post(self, request, *args, **kwargs):
+            
+
 # Create your views here.
