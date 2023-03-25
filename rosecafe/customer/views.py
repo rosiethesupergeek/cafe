b2c6c1f01e4337a3654346db46499c6a0657b42d
rosiethesupergeek <rosiethesupergeek.dev@gmail.com>
2023-03-25 08:11:18 +0000

    add html and connected everything

--- rosecafe/customer/views.py
+++ rosecafe/customer/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views import View
+from .models import MenuItem, Category, OrderModel
 
 class Index(View):
     def get(self, request, *args, **kwargs):
@@ -29,6 +30,36 @@ class Order(View):
         return render(request, 'customer/order.html', context)
 
         def post(self, request, *args, **kwargs):
-            
+            order_items = {
+                'items':[]
+            }
 
+            items = request.POST.getlist('items[]')
+
+            for item in items:
+                menu_item = MenuItem.objects.get(pk=int(item))
+                item_data = {
+                    'id' : menu_item.pk,
+                    'name': menu_item.name,
+                    'price': menu_item.price
+                }
+
+                order_items['items'].append(item_data)
+
+                price = 0
+                item_ids = []
+
+            for item in order_items['items']:
+                price += item['price']
+                item_ids.append(item['id'])
+
+                order = OrderModel.objects.create(price=price)
+                order.items.add(*item_ids)
+
+                context = {
+                    'items': order_items['items'],
+                    'price': price
+                }
+
+                return render(request, 'customer/order_confirmation.html', context)
 # Create your views here.
