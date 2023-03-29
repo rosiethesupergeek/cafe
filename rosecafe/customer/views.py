2dbbf0d4aa113857764096dce15cae9c2612d8a1
rosiethesupergeek <rosiethesupergeek.dev@gmail.com>
2023-03-29 16:21:23 +0000

    put together payment page

--- rosecafe/customer/views.py
+++ rosecafe/customer/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
+from django.core.mail import send_mail
 from .models import MenuItem, Category, OrderModel
 
 class Index(View):
@@ -29,52 +30,85 @@ class Order(View):
         #render the template
         return render(request, 'customer/order.html', context)
 
-        def post(self, request, *args, **kwargs):
-            name = request.POST.get('name')
-            email = request.POST.get('email')
-            street = request.POST.get('street')
-            city = request.POST.get('city')
-            state = request.POST.get('state')
-            zip_code = request.POST.get('zip_code')
-                        
-            order_items = {
-                'items':[]
+    def post(self, request, *args, **kwargs):
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        street = request.POST.get('street')
+        city = request.POST.get('city')
+        state = request.POST.get('state')
+        zip_code = request.POST.get('zip')
+                    
+        order_items = {
+            'items':[]
+        }
+
+        items = request.POST.getlist('items[]')
+
+        for item in items:
+            menu_item = MenuItem.objects.get(pk=int(item))
+            item_data = {
+                'id' : menu_item.pk,
+                'name': menu_item.name,
+                'price': menu_item.price
             }
 
-            items = request.POST.getlist('items[]')
+            order_items['items'].append(item_data)
 
-            for item in items:
-                menu_item = MenuItem.objects.get(pk=int(item))
-                item_data = {
-                    'id' : menu_item.pk,
-                    'name': menu_item.name,
-                    'price': menu_item.price
-                }
+        price = 0
+        item_ids = []
 
-                order_items['items'].append(item_data)
+        for item in order_items['items']:
+            price += item['price']
+            item_ids.append(item['id'])
 
-                price = 0
-                item_ids = []
+        order = OrderModel.objects.create(
+            price=price,
+            name = name,
+            email= email,
+            street=street,
+            state=state,
+            city=city,
+            zip_code=zip_code)
+        
+        order.items.add(*item_ids)
 
-            for item in order_items['items']:
-                price += item['price']
-                item_ids.append(item['id'])
+        # After everything is done, send confirmation email to the user
 
-                order = OrderModel.objects.create(
-                    price=price,
-                    name = name,
-                    email= email,
-                    street=street,
-                    state=state,
-                    city=city,
-                    zip_code=zip_code
-                    )
-                order.items.add(*item_ids)
+        body = ('Thank you for your order! Your food is being made and will be delivered soon!\n'
+            f'Your total: {price}\n'
+            'Thank you again for your order!')
+        
+        send_mail(
+            'Thank you for your order!',
+            body,
+            'example@example.com',
+            [email],
+            fail_silently= False
+            )
 
-                context = {
-                    'items': order_items['items'],
-                    'price': price
-                }
+        context = {
+            'items': order_items['items'],
+            'price': price
+        }
+        return redirect('order-confirmation', pk=order.pk)
 
-                return render(request, 'customer/order_confirmation.html', context)
-# Create your views here.
+
+class OrderConfirmation(View):
+    def get(self, request, pk, *args, **kwargs):
+        order = OrderModel.objects.get(pk=pk)
+
+        context = {
+            'pk': order.pk,
+            'items': order.items,
+            'price': order.price,
+        }
+
+        return render(request, 'customer/order_confirmation.html', context)
+    
+    def post(self, request, pk, *args, **kwargs):
+        print(request.body)
+
+
+class OrderPayConfirmation(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'customer/order_pay_confirmation.html')
